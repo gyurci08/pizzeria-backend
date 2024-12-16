5ea25ae5870d0e45fbcc34016a3c8a254a47dc56
gyoergy.jandzso@t-systems.com <haver1036@gmail.com>
2024-12-16 12:32:32 +0100

    Init

---
+++ src/main/java/hu/jandzsogyorgy/pizzeriabackend/PizzeriaBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.jandzsogyorgy.pizzeriabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzeriaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/jandzsogyorgy/pizzeriabackend/PizzeriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.jandzsogyorgy.pizzeriabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
